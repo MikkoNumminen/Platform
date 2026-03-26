33e26aeaf2050d0e649912f58600d41ef409286b
mikko numminen <mikko@mikkos-MacBook-Pro.local>
2026-03-27 00:39:55 +0200

    style(web): format middleware.ts with prettier

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -16,7 +16,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/((?!api/auth|auth/signin|survey|_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ["/((?!api/auth|auth/signin|survey|_next/static|_next/image|favicon.ico).*)"],
 };
